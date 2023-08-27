9d9d71d5426a22b7f1b1645fa9e8aaceac3c914f
Xheraa <mathiaswguldfeldt@gmail.com>
2023-08-27 05:06:11 +0200

    Added osu! API integration

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,14 @@
 const { Events } = require('discord.js');
+const { auth } = require('osu-api-extended');
+const { osuAppId, osuClientSecret } = require('../config.json');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
+        // Auth via client
+        await auth.login_lazer(osuAppId, osuClientSecret);
+        console.log(`Logged into osu! API as Xhera`);
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
